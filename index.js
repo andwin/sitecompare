c88badf3687b0ba24f375b5c96e8ec14c457ef35
Anders Windelhed <anders.windelhed@valtech.se>
2016-07-08 17:19:51 +0200

    Index file to fetch content, clean it and then compare it

---
+++ index.js
@@ -0,0 +1,22 @@
+const configParser = require('./lib/config'),
+      fetch = require('./lib/fetch'),
+      clean = require('./lib/clean'),
+      compare = require('./lib/compare');
+
+let configFile = process.argv[2];
+let config = configParser.parseFile(configFile);
+
+let fetcher = fetch(config.urls);
+
+for (let urlContent of fetcher) {
+  Promise.all([urlContent.actual.content, urlContent.expected.content]).then(values => {
+    let [actualContent, expectedContent] = values;
+
+    actualContent = clean(actualContent, config.removeContent.actual);
+    expectedContent = clean(expectedContent, config.removeContent.expected);
+
+    console.log();
+    console.log('Comparing ' + urlContent.actual.url + ' against ' + urlContent.expected.url);
+    compare(actualContent, expectedContent);
+  });
+}
